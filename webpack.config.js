d9e55d5d3b617c74e6ad74f0e3cdfdcbfa328b60
Kyle Johnson <zion4to6@gmail.com>
2021-02-23 17:53:48 -0800

    Initial webpack config setup, untested

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  mode: "development",
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
