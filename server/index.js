d96a5eb98cd133ae738764fd8050845799ba22b8
Kyle Johnson <zion4to6@gmail.com>
2021-02-25 18:00:19 -0800

    Added body parser

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ const app = express();
 const port = 3000;
 
 app.use('/static', express.static(path.join(__dirname, '..', 'public')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
