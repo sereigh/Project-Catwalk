158770d118a9b5b4d58102f65868bdff65d92511
Krista <kb9286@gmail.com>
2021-02-26 19:41:18 -0600

    test cleanup

--- client/index.js
+++ client/index.js
@@ -1,10 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var App = () => (
-  <div>
-    <h1>Hello</h1>
-  </div>
-);
-
 ReactDOM.render(<div>Hello, world!</div>, document.getElementById('app'));
