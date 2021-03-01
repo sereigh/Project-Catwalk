72d1eec78ab7ef433e18245abaf9da4dba73f527
Laura Nielsen <lnielsen.wi@gmail.com>
2021-03-01 14:47:59 -0600

    Axios requests in Client>App

--- client/App.jsx
+++ client/App.jsx
@@ -1,20 +1,63 @@
 import React from 'react';
+import Axios from 'axios';
+import Overview from './Overview/Overview'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      productList: {},
+      productId: 1
+      selectProductId: 1,
+      selectProductInfo: {}
+    };
+    this.retrieveAllProductInfo = this.retrieveAllProductInfo.bind(this);
+    this.retrieveSelectProductInfo = this.retrieveSelectProductInfo.bind(this);
+  }
+
+  retrieveAllProductInfo() {
+    axios
+      .get(`/product`)
+      .then((data) => {
+        this.setState({
+          productList: data
+        })
+      })
+      .catch((error) => {
+        console.log('Get total product list failed...', error);
+      })
+  }
+
+  retrieveSelectProductInfo() {
+    axios
+      .get(`/product/${this.state.selectProductId}`)
+      .then((data) => {
+        this.setState({
+          selectProductInfo: data
+        })
+      })
+      .catch((error) => {
+        console.log('Get product data by id failed...', error);
+      })
+  }
+
+  componentDidMount() {
+    this.retrieveSelectProductInfo();
+    this.retrieveAllProductInfo();
   }
 
   render() {
     return (
-      <>
+      <div>
         <span>Hello, world!</span>
-        {/* <Overview />
-        <RelatedItemsAndComparison />
-        <QuestionsAndAnswers />
-        <RatingsAndReviews /> */}
-      </>
+        <p>{this.state.selectProductId}</p>
+        <br></br>
+        <p>{this.state.selectProductInfo}</p>
+        {/* <Overview /> */}
+        {/* <RelatedItemsAndComparison /> */}
+        {/* <QuestionsAndAnswers /> */}
+        {/* <RatingsAndReviews /> */}
+      </div>
     );
   }
 }
