023c05e866a01f470b5590a696e38f6655ef0401
Krista <kb9286@gmail.com>
2021-03-01 13:04:56 -0600

    excused lifecycle methods from the use of this in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,5 +33,17 @@ module.exports = {
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "no-console": "off",
+    "class-methods-use-this": ["error", {
+      "expectMethods": [
+        "render",
+        "getInitialState",
+        "getDefaultProps",
+        "getChildContext",
+        "componentDidMount",
+        "shouldComponentUpdate",
+        "componentDidUpdate",
+        "componentWillUnmount",
+      ],
+    }],
   },
 };
