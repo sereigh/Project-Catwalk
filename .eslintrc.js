610fb13189b52b00fa92cb2a4b2a20c0f9dd0326
Krista <kb9286@gmail.com>
2021-03-01 14:27:58 -0600

    fixed filepath error and altered keyword this to off

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,17 +33,6 @@ module.exports = {
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "no-console": "off",
-    "class-methods-use-this": ["error", {
-      "expectMethods": [
-        "render",
-        "getInitialState",
-        "getDefaultProps",
-        "getChildContext",
-        "componentDidMount",
-        "shouldComponentUpdate",
-        "componentDidUpdate",
-        "componentWillUnmount",
-      ],
-    }],
+    "class-methods-use-this": "off",
   },
 };
