e0ad5433622b365e5569d19b843ece3df1e4e975
Laura Nielsen <lnielsen.wi@gmail.com>
2021-03-02 13:50:11 -0600

    App updated OverviewContainer concerns

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 
+import OverviewContainer from './Overview/OverviewContainer.jsx';
 import ReviewsListContainer from './Rating&Reviews/ReviewsListContainer.jsx';
 import QuestionsAndAnswers from './Questions&Answers/QuestionsAndAnswers.jsx';
 import RelatedListContainer from './RelatedItems&Comparison/RelatedListContainer.jsx';
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      productId: 17762,
       productList: [
         {"id":17762,"campus":null,"name":null,"slogan":null,"description":null,"category":null,"default_price":null,"created_at":null,"updated_at":null}
       ],
-      productId: 17762,
-      selectProductId: 17762,
+      selectProductId: 17763,
       selectProductInfo: [
-        {"id":17762,"campus":null,"name":null,"slogan":null,"description":null,"category":null,"default_price":null,"created_at":null,"updated_at":null}
+        {"id":17763,"campus":null,"name":null,"slogan":null,"description":null,"category":null,"default_price":null,"created_at":null,"updated_at":null}
       ]
     };
     this.retrieveAllProductInfo = this.retrieveAllProductInfo.bind(this);
@@ -56,11 +56,9 @@ class App extends React.Component {
   }
 
   render() {
-    const { selectProductId } = this.state;
-    const { selectProductInfo } = this.state;
-    const { productId } = this.state;
-    const { productList } = this.state;
-    console.log('render selectProductInfo:', selectProductInfo);
+    const { productId, productList, selectProductId, selectProductInfo } = this.state;
+    // console.log('render productList:', productList);
+    // console.log('render selectProductInfo:', selectProductInfo);
     return (
       <div>
         <span>Hello, world!</span>
@@ -71,7 +69,11 @@ class App extends React.Component {
         <p>{productId}</p>
         <br />
         <p>{productList[0].category}</p>
-        {/* <Overview /> */}
+        <OverviewContainer
+          selectProductId={selectProductId}
+          selectProductInfo={selectProductInfo}
+          retrieveSelectProductInfo={this.retrieveSelectProductInfo}
+        />
         <RelatedListContainer />
         <QuestionsAndAnswers />
         <ReviewsListContainer />
