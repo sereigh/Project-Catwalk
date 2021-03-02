47b0f3f3d0a75cac2c4fc93f58c08abbf2d521b4
Krista <kb9286@gmail.com>
2021-03-02 15:00:32 -0600

    created sample date

---
+++ client/Questions&Answers/utility/answers.js
@@ -0,0 +1,5 @@
+const answers = [
+  { id: 1, text: '10/10 would reccomend', helpful: 20, user: 'User1234', date: 'January 1, 2021' }, { id: 2, text: '10/10 would reccomend', helpful: 5, user: 'User1234', date: 'January 2, 2021' }, { id: 3, text: '10/10 would reccomend', helpful: 2, user: 'User1234', date: 'January 3, 2021' }, { id: 4, text: '10/10 would reccomend', helpful: 0, user: 'User1234', date: 'January 4, 2021' }
+];
+
+export default answers;
---
+++ client/Questions&Answers/utility/questions.js
@@ -0,0 +1,5 @@
+const questions = [
+  { id: 1, text: 'Who what which when where why and how?', helpful: 20, user: 'User1234', date: 'January 1, 2021' }, { id: 2, text: 'Who what which when where why and how?', helpful: 5, user: 'User1234', date: 'January 2, 2021' }, { id: 3, text: 'Who what which when where why and how?', helpful: 2, user: 'User1234', date: 'January 3, 2021' }, { id: 4, text: 'Who what which when where why and how?', helpful: 0, user: 'User1234', date: 'January 4, 2021' }
+];
+
+export default questions;
