891e894c6fc7c9eb476e95ba69acf32e3406c2f4
Krista <kb9286@gmail.com>
2021-03-01 18:34:34 -0600

    qa container now has state to communicate current product

--- client/Questions&Answers/QuestionsAndAnswers.jsx
+++ client/Questions&Answers/QuestionsAndAnswers.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 
 class QuestionsAndAnswers extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
+    this.state = {
+      product: 18076
+    };
   }
+
   render() {
+    const {product} = this.state
 
     return (
       <div className="qaContainer" style={{ border: 'solid black thin' }}>
         Container
         <div className="qaHeader" style={{ border: 'solid black thin' }}>
           Header
-    </div>
+        </div>
         <div className="qaSearch" style={{ border: 'solid black thin' }}>
           Search
-    </div>
+        </div>
         <div className="qaList" style={{ border: 'solid black thin' }}>
           List
-    </div>
+        </div>
       </div>
     );
   }
