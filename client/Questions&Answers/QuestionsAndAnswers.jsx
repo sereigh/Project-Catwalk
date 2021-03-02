f24b624a4e6515687a9379bcfd2074f760af8027
Krista <kb9286@gmail.com>
2021-03-01 18:27:46 -0600

    qa container now has class

--- client/Questions&Answers/QuestionsAndAnswers.jsx
+++ client/Questions&Answers/QuestionsAndAnswers.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
-const QuestionsAndAnswers = () => (
-  <div className="qaContainer" style={{border: 'solid black thin'}}>
-    Container
-    <div className="qaHeader" style={{border: 'solid black thin'}}>
-      Header
+class QuestionsAndAnswers extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  render() {
+
+    return (
+      <div className="qaContainer" style={{ border: 'solid black thin' }}>
+        Container
+        <div className="qaHeader" style={{ border: 'solid black thin' }}>
+          Header
     </div>
-    <div className="qaSearch" style={{border: 'solid black thin'}}>
-      Search
+        <div className="qaSearch" style={{ border: 'solid black thin' }}>
+          Search
     </div>
-    <div className="qaList" style={{border: 'solid black thin'}}>
-      List
+        <div className="qaList" style={{ border: 'solid black thin' }}>
+          List
     </div>
-  </div>
-);
+      </div>
+    );
+  }
+};
 
 export default QuestionsAndAnswers;
