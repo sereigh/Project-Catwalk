26ce9e12338d53190195eef4ac0c9c18ed07200a
Krista <kb9286@gmail.com>
2021-03-01 18:22:49 -0600

    qa container now renders placeholders for content

--- client/Questions&Answers/QuestionsAndAnswers.jsx
+++ client/Questions&Answers/QuestionsAndAnswers.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const QuestionsAndAnswers = () => (
-  <h1>Hello</h1>
+  <div className="qaContainer" style={{border: 'solid black thin'}}>
+    Container
+    <div className="qaHeader" style={{border: 'solid black thin'}}>
+      Header
+    </div>
+    <div className="qaSearch" style={{border: 'solid black thin'}}>
+      Search
+    </div>
+    <div className="qaList" style={{border: 'solid black thin'}}>
+      List
+    </div>
+  </div>
 );
 
 export default QuestionsAndAnswers;
