1d59014720f6f692cd3d945411ca059d1f8e1985
Kyle Johnson <zion4to6@gmail.com>
2021-03-03 19:53:11 -0800

    Wrote function to generate appropriate characteristic description

--- client/Rating&Reviews/CharacteristicRate.jsx
+++ client/Rating&Reviews/CharacteristicRate.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CharacteristicRate = ({characteristic, handleCharacteristicRate}) => (
-  <div>
-    {characteristic}
-    <input type='radio' name={`${characteristic}-rating`} value='1' onChange={event => handleCharacteristicRate(event, characteristic)} />
-    <input type='radio' name={`${characteristic}-rating`} value='2' onChange={event => handleCharacteristicRate(event, characteristic)} />
-    <input type='radio' name={`${characteristic}-rating`} value='3' onChange={event => handleCharacteristicRate(event, characteristic)} />
-    <input type='radio' name={`${characteristic}-rating`} value='4' onChange={event => handleCharacteristicRate(event, characteristic)} />
-    <input type='radio' name={`${characteristic}-rating`} value='5' onChange={event => handleCharacteristicRate(event, characteristic)} />
-  </div>
-)
+import generateDescription from './generateDescription';
+
+class CharacteristicRate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: 'None Selected'
+    }
+  }
+
+  render() {
+    const {characteristic, handleCharacteristicRate} = this.props;
+    const {description} = this.state;
+
+    return (
+      <div>
+        <div className='description'>
+          {description}
+        </div>
+        {characteristic}
+        <input type='radio' name={`${characteristic}-rating`} value='1' onChange={event => handleCharacteristicRate(event, characteristic)} />
+        <input type='radio' name={`${characteristic}-rating`} value='2' onChange={event => handleCharacteristicRate(event, characteristic)} />
+        <input type='radio' name={`${characteristic}-rating`} value='3' onChange={event => handleCharacteristicRate(event, characteristic)} />
+        <input type='radio' name={`${characteristic}-rating`} value='4' onChange={event => handleCharacteristicRate(event, characteristic)} />
+        <input type='radio' name={`${characteristic}-rating`} value='5' onChange={event => handleCharacteristicRate(event, characteristic)} />
+      </div>
+    )
+  }
+}
 
 CharacteristicRate.propTypes = {
   characteristic: PropTypes.string.isRequired,
