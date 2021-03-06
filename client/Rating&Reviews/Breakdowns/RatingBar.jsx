8050bb38183a7c6c2069ea1b784015f15c7dba6e
Kyle Johnson <zion4to6@gmail.com>
2021-03-06 01:29:45 -0800

    Refactored rating bars to be simpler and more responsive

--- client/Rating&Reviews/Breakdowns/RatingBar.jsx
+++ client/Rating&Reviews/Breakdowns/RatingBar.jsx
@@ -3,33 +3,22 @@ import PropTypes from 'prop-types';
 
 const RatingBar = ({percent}) => {
   const containerStyle = {
-    'position': 'relative',
-    'top': '-50%'
+    'width': '80%',
+    'height': '10px',
+    'transform': 'translate(5px, 50%)',
+    'backgroundColor': 'gray'
   }
 
   const filledStyle = {
     'zIndex': '1',
-    'position': 'absolute',
-    'left': '0',
-    'transform': 'translate(10px, 50%)',
-    'width': `${percent}px`,
+    'width': `${percent}%`,
     'height': '10px',
     'backgroundColor': 'green'
-  }
-
-  const emptyStyle = {
-    'position': 'absolute',
-    'left': '0',
-    'transform': 'translate(10px, 50%)',
-    'width': '100px',
-    'height': '10px',
-    'backgroundColor': 'gray'
   }
 
   return (
     <div style={containerStyle}>
       <div style={filledStyle} />
-      <div style={emptyStyle} />
     </div>
   )
 }
