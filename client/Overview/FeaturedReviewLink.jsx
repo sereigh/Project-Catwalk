2a3252bf5df981b6d97b02bb032b44c8e28b1d75
Laura Nielsen <lnielsen.wi@gmail.com>
2021-03-06 22:06:12 -0600

    OvC FeaturedReviewLink Stars component commentary from KJ

--- client/Overview/FeaturedReviewLink.jsx
+++ client/Overview/FeaturedReviewLink.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import Stars from 'client/SharedComponents/Stars.jsx';
 
+// Kyle: I did also create a Stars component that may be of use to you. It's in the SharedComponents folder. All you have to pass it is a rating number as a prop, and it will give you an appropriate amount of filled stars.
+
 function FeaturedReviewLink() {
 // const FeaturedReviewLink = () => {
 // error: Unexpected block statement surrounding arrow body; move the returned value immediately after the `=>`  arrow-body-style
