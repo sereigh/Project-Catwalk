1061aff6207c10f091c8635201228da8277cf975
Laura Nielsen <lnielsen.wi@gmail.com>
2021-03-06 22:04:07 -0600

    OvC FeaturedProduct category displays capitalized

--- client/Overview/FeaturedReviewLink.jsx
+++ client/Overview/FeaturedReviewLink.jsx
@@ -1,11 +1,14 @@
-// import React from 'react';
+import React from 'react';
+// import Stars from 'client/SharedComponents/Stars.jsx';
 
+function FeaturedReviewLink() {
 // const FeaturedReviewLink = () => {
-//   return (
-//     <div id="" />
+// error: Unexpected block statement surrounding arrow body; move the returned value immediately after the `=>`  arrow-body-style
+  return (
+    <div id="productAreaStars">
+      <h5>Placeholder: [*****] Read all reviews</h5>
+    </div>
+  );
+}
 
-
-//   );
-// }
-
-// export default FeaturedReviewLink;
+export default FeaturedReviewLink;
