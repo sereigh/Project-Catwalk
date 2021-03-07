d669570e39e1d1bdb5794df18aa53f14b49e52a1
Laura Nielsen <lnielsen.wi@gmail.com>
2021-03-06 21:03:31 -0600

    OvC DescriptionBanner displays item feature checkmark list

--- client/Overview/DescriptionBanner.jsx
+++ client/Overview/DescriptionBanner.jsx
@@ -2,43 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const DescriptionBanner = (props) => {
-  // selectProductName={selectProductInfo.name} viaOvC
-  // const { selectProductInfo, stylesLoaded } = props;
   const { selectProductInfo } = props;
-  console.log('DescriptionBanner return selectProductInfo:', selectProductInfo);
-  // if ( !stylesLoaded ) {
-  //   return (
-  //     <p>Loading styles</p>
-  //   );
-  // }
-  // if ( stylesLoaded ) {
-  // } else {
-    return (
-      <div className="descriptionBanner">
-        <div id="descriptionAreaLeft">
-          <h4>{selectProductInfo.slogan}</h4>
-          <p>{selectProductInfo.description}</p>
-          {/* <p>{selectProductInfo.overview}</p> */}
-          {/* Business Requirements Document (page5): Product Overview: This free form text field may exist on some items. If it is available it should be displayed. */}
-        </div>
-        <div id="hrVertical">
-          <hr />
-        </div>
-        <div id="descriptionAreaRight">
-          <ul>
-            {selectProductInfo.features.map((item) => (
-              <li key={item.feature} className="checkIt">
-                <span>
-                  ✓
-                  {item.feature}
-                </span>
-              </li>
-            ))}
-          </ul>
-        </div>
+  // console.log('DescriptionBanner_return X:', X);
+  return (
+    <div className="descriptionBanner">
+      <div id="descriptionAreaLeft">
+        <h4>{selectProductInfo.slogan}</h4>
+        <p>{selectProductInfo.description}</p>
+        <p>{selectProductInfo.overview}</p>
+        {/* Business Requirements Document (page5): Product Overview: This free form text field may exist on some items. If it is available it should be displayed. */}
       </div>
-    );
-  // }
+      <div id="hrVertical">
+        <hr />
+      </div>
+      <div id="descriptionAreaRight">
+        <ul>
+          {selectProductInfo.features.map((item) => (
+            <li key={item.feature} className="checkIt">
+              <span>
+                ✓
+                {item.feature}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 
 }
 
@@ -59,7 +49,6 @@ DescriptionBanner.propTypes = {
       "map": PropTypes.node
     }))
   }).isRequired
-  // stylesLoaded: PropTypes.bool.isRequired
 }
 
 export default DescriptionBanner;
