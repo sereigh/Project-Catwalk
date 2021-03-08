b04004963ef0b23b257e291f66f5efd0a7fe36b1
Kyle Johnson <zion4to6@gmail.com>
2021-03-08 12:46:17 -0800

    Small logic fix

--- client/Rating&Reviews/ReviewsListContainer.jsx
+++ client/Rating&Reviews/ReviewsListContainer.jsx
@@ -50,7 +50,7 @@ class ReviewsListContainer extends React.Component {
         </span>
         <input type='text' placeholder='Search Reviews...' onChange={handleSearch} />
         <ReviewsList minimized={minimized} reviews={reviews} />
-        {totalReviews > 3 &&
+        {totalReviews >= 3 &&
           <button type='button' onClick={this.handleView}>{minimized ? 'More Reviews' : 'Fewer Reviews'}</button>}
         <WriteReview characteristics={characteristics} productId={productId} productName={productName} handleSort={handleSort} selected={selected} />
       </div>
