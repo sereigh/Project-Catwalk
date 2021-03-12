5b3f60464e1a5fb51ef3e5108ed9f2e6a16227b0
Laura Nielsen <lnielsen.wi@gmail.com>
2021-03-12 06:54:57 -0600

    OvC styling on hover

--- client/Overview/DescriptionOutbound.jsx
+++ client/Overview/DescriptionOutbound.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+// FEC/public/icons/iconfinder_Logo_facebook.png
+// FEC/client/Overview/DescriptionOutbound.jsx
+
+// import logoFacebook from '../../public/icons/iconfinder_Logo_facebook.png';
+
+// ERROR in ./public/icons/iconfinder_Logo_facebook.png 1:0
+// Module parse failed: Unexpected character '�' (1:0)
+// You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
+
 function DescriptionOutbound() {
   return (
     <div id="descriptionOutbound">
       <span id="shareToFacebook">
         <a href="https://www.Facebook.com">[ Fb ]</a>
       </span>
+      {/* <span id="shareToFacebook">
+        <a href="https://www.Facebook.com">
+          <img src={logoFacebook} alt="logoFacebook" id="logoFacebook" />
+        </a>
+      </span> */}
       <span id="shareToTwitter">
         <a href="https://Twitter.com">[ Tw ]</a>
       </span>
