e79b1fd8e9e0a0b76c5e29c52a34f1624fc9bf7e
Krista <kb9286@gmail.com>
2021-03-13 16:30:58 -0600

    query now gets marked but it shows as a string instead of rendering the html

--- client/Questions&Answers/Utility.jsx
+++ client/Questions&Answers/Utility.jsx
@@ -2,34 +2,34 @@ const sortQuestions = (response) => Object.values(response.data).sort((a, b) => 
 
 const sortAnswers = (data) => Object.values(data).sort((a, b) => b.helpfulness - a.helpfulness);
 
-// const filterSearch = (list, query) => list.filter((question) => {
-//   if (question.question_body.toLowerCase().includes(query.toLowerCase()) ||
-//     Object.values(question.answers).some((answer) => answer.body.toLowerCase().includes(query.toLowerCase()))
-//   ) {
-//     return true
-//   }
-//   return false
-// })
+const filterSearch = (list, query) => list.filter((question) => {
+  if (question.question_body.toLowerCase().includes(query.toLowerCase()) ||
+    Object.values(question.answers).some((answer) => answer.body.toLowerCase().includes(query.toLowerCase()))
+  ) {
+    return true
+  }
+  return false
+})
 
 const highlight = (body, query) => body.replace(query, `<mark>${query}</mark>`)
 
-const filterAnswers = (answers, query) => Object.values(answers).some((answer) => {
-    if (answer.body.toLowerCase().includes(query.toLowerCase())) {
-      highlight(answer.body, query)
-      return true
-    }
-    return false
-  })
+// const filterAnswers = (answers, query) => Object.values(answers).some((answer) => {
+//     if (answer.body.toLowerCase().includes(query.toLowerCase())) {
+//       answer.body = highlight(answer.body, query)
+//       return true
+//     }
+//     return false
+//   })
 
-const filterSearch = (list, query) => list.filter((question) => {
-    if (question.question_body.toLowerCase().includes(query.toLowerCase())) {
-      highlight(question.question_body, query)
-      if (filterAnswers(question.answers, query)) { return true }
-      return true
-    }
-    if (filterAnswers(question.answers, query)) { return true }
-    return false
-  })
+// const filterSearch = (list, query) => list.filter((question) => {
+//     if (question.question_body.toLowerCase().includes(query.toLowerCase())) {
+//       question.question_body = highlight(question.question_body, query)
+//       if (filterAnswers(question.answers, query)) { return true }
+//       return true
+//     }
+//     if (filterAnswers(question.answers, query)) { return true }
+//     return false
+//   })
 
 const findPath = (type, id, option) => {
   let path;
